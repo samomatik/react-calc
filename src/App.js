88e2f5a622f3b92b1e4ce3bcbc932ab6f96f72db
Samitha Padukkage Don <samitha.312@gmail.com>
2019-07-24 11:03:39 +1000

    Added files

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import ReactFCCtest from 'react-fcctest';
+import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeypadComponent from './components/KeypadComponent';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if(button === "=") {
+      this.calculate()
+    }
+    else if(button === "C") {
+      this.reset()
+    }
+    else if(button === "CE") {
+      this.backspace()
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        // eslint-disable-next-line
+        result: (eval(this.state.result) || "" ) + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: "7"
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calculator-body">
+          <h1>Simple Calculator</h1>
+          <ResultComponent result={this.state.result} />
+          <KeypadComponent onClick={this.onClick} />
+          <ReactFCCtest />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/ResultComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ResultComponent extends Component {
+    render () {
+        let {result} = this.props;
+        return (
+            <div id="display" className="result">
+                <p>{result}</p>
+            </div>
+        );
+    }
+}
+
+export default ResultComponent;
