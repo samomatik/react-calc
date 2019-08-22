8edd7a4cd54cd52ce123ad4d8ab29c08a95074f0
Samitha Padukkage Don <samitha.312@gmail.com>
2019-08-22 10:17:54 +1000

    Updates

---
+++ precache-manifest.3639e6c0ef9673f550a3d61f584759d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6b5b2fe19d872daf2168f165e40b9b",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "b4aedfe2bf1075fe0180",
+    "url": "/react-calc/static/css/main.316febde.chunk.css"
+  },
+  {
+    "revision": "5aa9bd2acd03f9b11f48",
+    "url": "/react-calc/static/js/2.75e2f876.chunk.js"
+  },
+  {
+    "revision": "b4aedfe2bf1075fe0180",
+    "url": "/react-calc/static/js/main.7598cda4.chunk.js"
+  },
+  {
+    "revision": "48eedb427e7cd8bd6dd1",
+    "url": "/react-calc/static/js/runtime~main.0d723d41.js"
+  }
+]);
